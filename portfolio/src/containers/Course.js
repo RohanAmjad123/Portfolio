09c0391ace76270089e95aadf13d25ece444fa5e
Rohan Amjad <rohanamjad123@gmail.com>
2021-11-16 01:09:42 -0700

    removed cardmedia from course

--- portfolio/src/containers/Course.js
+++ portfolio/src/containers/Course.js
@@ -1,13 +1,11 @@
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
 
 function Course(props) {
   return (
     <Grid item lg={3} md={4} sm={12} xs={12}>
       <Card variant="outlined" raised={true}>
-        <CardMedia component="img" image={props.image} alt="course image" />
         <CardHeader title={props.title} subheader={props.desc} />
       </Card>
     </Grid>
--- portfolio/src/pages/Courses.js
+++ portfolio/src/pages/Courses.js
@@ -42,51 +42,42 @@ function Courses() {
             <Course
               title="CPSC 441"
               desc="Computer Networks"
-              image={images["computer_networks.jpg"].default}
             />
             <Course
               title="ENCM 511"
               desc="Embedded Systems Interfacing"
-              image={images["embedded_systems_interfacing.jpg"].default}
             />
             <Course
               title="ENSF 480"
               desc="Principles of Software Design"
-              image={images["white.jpg"].default}
             />
           </Semester>
           <Semester title="Spring 2021">
             <Course
               title="CPSC 457"
               desc="Principles of Operating Systems"
-              image={images["white.jpg"].default}
             />
             <Course
               title="ENGG 513"
               desc="Roles and Responsibilities of Prof. Engineers in Society"
-              image={images["white.jpg"].default}
             />
           </Semester>
           <Semester title="Winter 2021">
             <Course
               title="CPSC 471"
               desc="Database Management Systems"
-              image={images["white.jpg"].default}
             />
             <Course
               title="ENCM 369"
               desc="Computer Organization"
-              image={images["white.jpg"].default}
             />
             <Course
               title="ENSF 409"
               desc="Principles of Software Development"
-              image={images["white.jpg"].default}
             />
             <Course
               title="SENG 471"
               desc="Software Requirements Engineering"
-              image={images["white.jpg"].default}
             />
           </Semester>
         </AccordionDetails>
@@ -109,29 +100,24 @@ function Courses() {
             <Course
               title="ENEL 353"
               desc="Digital Circuits"
-              image={images["white.jpg"].default}
             />
             <Course
               title="MATH 271"
               desc="Discrete Mathematics"
-              image={images["white.jpg"].default}
             />
             <Course
               title="SAST 203"
               desc="Understanding South Asia"
-              image={images["white.jpg"].default}
             />
           </Semester>
           <Semester title="Summer 2020">
             <Course
               title="CPSC 319"
               desc="Data Structures and Algorithms"
-              image={images["white.jpg"].default}
             />
             <Course
               title="ENEL 327"
               desc="Signals and Transforms"
-              image={images["white.jpg"].default}
             />
             
           </Semester>
@@ -139,34 +125,28 @@ function Courses() {
             <Course
               title="ENSF 337"
               desc="Programming Fundamentals for Software and Computer Engineers"
-              image={images["white.jpg"].default}
             />
           </Semester>
           <Semester title="Winter 2020">
             <Course
               title="COMS 363"
               desc="Professional Technical Communication"
-              image={images["white.jpg"].default}
             />
             <Course
               title="ENGG 201"
               desc="Behaviour of Liquids, Gases, and Solids"
-              image={images["white.jpg"].default}
             />
             <Course
               title="ENGG 209"
               desc="Engineering Economics"
-              image={images["white.jpg"].default}
             />
             <Course
               title="ENGG 225"
               desc="Fundamentals of Electrical Circuits and Machines"
-              image={images["white.jpg"].default}
             />
             <Course
               title="ENGG 481"
               desc="Technology and Society"
-              image={images["white.jpg"].default}
             />
           </Semester>
         </AccordionDetails>
