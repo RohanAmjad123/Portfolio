33676c5822d88aaed675187d61ced0b485c3a013
Rohan Amjad <rohanamjad123@gmail.com>
2022-03-18 00:10:28 -0600

    added a simple fadeIn spring for fade in animation in Home.js

--- portfolio/src/sections/Home.js
+++ portfolio/src/sections/Home.js
@@ -1,10 +1,16 @@
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
+import { useSpring, animated } from "@react-spring/web";
 
 function Home() {
+  const fadeIn = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+  });
+
   return (
-    <Container sx={{ height: "100vh"}}>
+    <Container sx={{ height: "100vh" }}>
       <Grid
         container
         direction="column"
@@ -15,17 +21,19 @@ function Home() {
         sm={12}
         xs={12}
       >
-        <Grid item>
-          <Typography variant="h2">Rohan Amjad</Typography>
-        </Grid>
-        <Grid item>
-          <Typography variant="h4">Software Engineering, BSc</Typography>
-        </Grid>
-        <Grid item>
-          <Typography variant="h5">
-            Schulich School of Engineering | University of Calgary
-          </Typography>
-        </Grid>
+        <animated.div style={fadeIn}>
+          <Grid item>
+            <Typography variant="h2">Rohan Amjad</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="h4">Software Engineering, BSc</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="h5">
+              Schulich School of Engineering | University of Calgary
+            </Typography>
+          </Grid>
+        </animated.div>
       </Grid>
     </Container>
   );
